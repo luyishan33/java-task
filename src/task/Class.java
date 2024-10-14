2884e74357b03d4be481165ac71792f2d90b007e
luyishan33 <350548120@qq.com>
2024-10-14 23:13:36 +0800

    java task

---
+++ src/task/Class.java
@@ -0,0 +1,40 @@
+package task;
+
+import java.util.ArrayList;
+
+public class Class {
+    private String name;
+    private ArrayList<Group> groups;
+    private ArrayList<Student> students;
+
+    public Class(String name){
+        this.name=name;
+        this.groups=new ArrayList<>();
+        this.students=new ArrayList<>();
+    }
+
+    public void addGroup(Group group){
+        groups.add(group);
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+    public ArrayList<Group> getGroups(){
+        return groups;
+    }
+
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "Class{name='" + name + "',groups=" + groups +",students=" + students + "}";
+    }
+}
